402becd768b739b0031f993427c06643639c817b
Daniel S <iam@w-b.dev>
2022-02-21 11:41:39 -0400

    create stage in api gateway

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 exports.handler = async function (event, context) {
-  // console.log("ENVIRONMENT VARIABLES\n" + JSON.stringify(process.env, null, 2))
-  // console.log("EVENT\n" + JSON.stringify(event, null, 2))
-  // return context.logStreamName
   const {
     requestContext: {
       stage,
-      // domainPrefix,
       http: { method, path },
     },
     body,
-    // cookies,
-    // headers,
-    // stageVariables,
   } = event;
   if (method === "GET") {
     return {
