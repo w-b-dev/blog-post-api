1569298fecf90b4cc7f11c6dfe1cfcd629ab2691
Daniel Santos <iam@w-b.dev>
2022-02-23 21:55:22 -0400

    add `jq` formating

--- curl-get.sh
+++ curl-get.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-curl https://api-staging.w-b.dev --request GET --verbose
+# These require JQ https://stedolan.github.io/jq/tutorial/
+curl https://api-staging.w-b.dev --request GET --verbose | jq '.'
 # curl https://cg3a2ru4dk.execute-api.us-east-1.amazonaws.com/blog-post -v -X PUT -H "Content-Type: application/json" --data @curl_body.json
