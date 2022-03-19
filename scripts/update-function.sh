91c88d5483e0a618455d5733eecb9dd607343cb9
Daniel Santos <iam@w-b.dev>
2022-03-19 20:34:07 -0300

    make update script more resilient

--- scripts/update-function.sh
+++ scripts/update-function.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-rm function.zip
-zip function.zip index.js
+cd ../src || exit
+[ -f function.zip ] && rm function.zip
+[ -f index.js ] && zip function.zip index.js
+mv function.zip ../build
+cd ../build || exit
 aws lambda update-function-code --function-name  blog-post --zip-file fileb://function.zip
