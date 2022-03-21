2149ae448d86de362dabbb64d3ab82fbed18684f
Daniel Santos <iam@w-b.dev>
2022-03-20 23:33:42 -0300

    clean up scripts

--- scripts/curl-get.sh
+++ scripts/curl-get.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-# These require JQ https://stedolan.github.io/jq/tutorial/
 curl https://api-staging.w-b.dev --request GET --verbose | jq '.'
-# curl https://cg3a2ru4dk.execute-api.us-east-1.amazonaws.com/blog-post -v -X PUT -H "Content-Type: application/json" --data @curl_body.json
