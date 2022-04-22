ef61e3596197a56feb35c276eeec21620f6b876c
Daniel Santos <iam@w-b.dev>
2022-04-22 13:47:20 -0300

    prettier

--- src/delete.js
+++ src/delete.js
@@ -1,5 +1,5 @@
-const {types} = require("cassandra-driver");
-const {deleteByID} = require("./queries");
+const { types } = require("cassandra-driver");
+const { deleteByID } = require("./queries");
 const uuid = types.Uuid;
 
 /**
@@ -13,45 +13,45 @@ const uuid = types.Uuid;
  * @returns {{statusCode: number}|{tracing: {logStreamName, awsRequestId}, isDevEnvironment: string, message: string, body: any, stageVariables}}
  */
 const handlerDELETE = async (
-    clienteCassandra,
-    isDevEnvironment,
-    stageVariables,
-    queryStringParameters,
-    path,
-    context
+  clienteCassandra,
+  isDevEnvironment,
+  stageVariables,
+  queryStringParameters,
+  path,
+  context
 ) => {
-    // POST_ID
-    const id = queryStringParameters.post_id;
-    const timestamp = queryStringParameters.post_timestamp;
-    const creation_date = queryStringParameters.post_creation_date;
-    // INSERTS, UPDATES AND DELETES CAN BE BATCHED
-    // https://docs.datastax.com/en/developer/nodejs-driver/4.6/features/batch/
-    await clienteCassandra.execute(deleteByID, [creation_date, timestamp, id], {
-        prepare: true,
-    });
+  // POST_ID
+  const id = queryStringParameters.post_id;
+  const timestamp = queryStringParameters.post_timestamp;
+  const creation_date = queryStringParameters.post_creation_date;
+  // INSERTS, UPDATES AND DELETES CAN BE BATCHED
+  // https://docs.datastax.com/en/developer/nodejs-driver/4.6/features/batch/
+  await clienteCassandra.execute(deleteByID, [creation_date, timestamp, id], {
+    prepare: true,
+  });
 
-    if (isDevEnvironment) {
-        const {logStreamName, awsRequestId} = context;
-        // SHUTDOWN ANTES DE RETORNAR
-        await clienteCassandra.shutdown();
-        return {
-            isDevEnvironment: "🚧 ➲ 🚧 ➲ 🚧 ➲ 🚧",
-            message: `ID ${id} deletado`,
-            // queryStringParameters: queryStringParameters,
-            // stageVariables,
-            tracing: {
-                logStreamName,
-                awsRequestId,
-            },
-        };
-    }
+  if (isDevEnvironment) {
+    const { logStreamName, awsRequestId } = context;
     // SHUTDOWN ANTES DE RETORNAR
     await clienteCassandra.shutdown();
-    /* PUT has no return body. Put is IDEMPOTENT (several repeated requests should cause 1 transformation only)
-     * - if content was created, return 201
-     * - if content was modified, return 204 */
     return {
-        body: id,
+      isDevEnvironment: "🚧 ➲ 🚧 ➲ 🚧 ➲ 🚧",
+      message: `ID ${id} deletado`,
+      // queryStringParameters: queryStringParameters,
+      // stageVariables,
+      tracing: {
+        logStreamName,
+        awsRequestId,
+      },
     };
+  }
+  // SHUTDOWN ANTES DE RETORNAR
+  await clienteCassandra.shutdown();
+  /* PUT has no return body. Put is IDEMPOTENT (several repeated requests should cause 1 transformation only)
+   * - if content was created, return 201
+   * - if content was modified, return 204 */
+  return {
+    body: id,
+  };
 };
 exports.handler = handlerDELETE;
